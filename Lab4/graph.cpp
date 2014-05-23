90128c1e1942a5a09fa1660e829e84de04ed229c
Lovis <lovi.dahl@gmail.com>
2014-05-23 16:25:13 +0200

    cool version

--- Lab4/graph.cpp
+++ Lab4/graph.cpp
@@ -59,6 +59,50 @@ void Graph::removeEdge(int u, int v)
 void Graph::mstPrim() const
 {
     // *** TODO ***
+    int dist[size+1];
+    bool done[size+1];
+    int path[size+1];
+    // bool cloud[size+1];
+    
+    for(int i=1; i<size+1; i++){
+        done[i] = false;
+        dist[i] = INFINITY;
+        path[i] = 0;
+    }
+    
+    int nextNode= 1;
+    dist[nextNode] = 0;
+    done[nextNode] = true;
+    
+    bool allVisited = false;
+    
+    while(nextNode < 8){
+        //1. Adding a node to the cloud
+        cout << "nextNode is: " << nextNode << endl;
+        done[nextNode] = true;
+        int nextCloudMember = INFINITY;
+        //Loop through all connecting edges of the cloud
+        for(int i = 1; i < size+1; i++){
+            if(done[i]){
+                cout << " node " << i << " is searching.. " << endl;
+                Node *temp = array[i].getFirst();
+                while (temp != nullptr){
+                    if(!done[temp->vertex]){
+                        cout << " HEJ cloud: " << temp->vertex << endl;
+                        path[temp->vertex] = nextNode;
+                        dist[temp->vertex] = temp->weight;
+                        //Save closest neighbor to the cloud
+                        //kolla närmsta vägen till de grannar till noden som ej är visited
+                        //nextCloudMember = closestNeighbor;
+                    }
+                    temp = temp->next;
+                }
+            }
+            nextNode++;
+            //cout << "updated nextNode: " << nextNode << endl;
+            done[2] = done[3] = true;
+        }
+    }
 }
 
 // Kruskal's minimum spanning tree algorithm
