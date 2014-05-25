7609260b5fecce8d7da3970a0243f60b97a4231c
EmmaEdv <emmed608@student.liu.se>
2014-05-25 12:21:23 +0200

    We're done

---
+++ Lab4/dsets.cpp
@@ -0,0 +1,108 @@
+/*********************************************
+* file:	~\tnd004\lab\lab4b\dsets.cpp         *
+* remark: implementation of disjoint sets    *
+* file for students                          *
+**********************************************/
+
+
+#include <iostream>
+#include <iomanip>
+
+#include <cassert>
+
+using namespace std;
+
+#include "dsets.h"
+
+// -- CONSTRUCTORS
+
+DSets::DSets(int theSize)
+{
+    assert(theSize > 0);
+    size = theSize;
+    array = new int[size + 1];
+    init();
+}
+
+// -- DESTRUCTOR
+
+DSets::~DSets()
+{
+    delete[] array;
+}
+
+// -- MEMBER FUNCTIONS
+
+// create initial sets
+void DSets::init()
+{
+    for (int i = 1; i <= size; i++)
+    {
+        array[i] = -1;
+    }
+}
+
+// join sets named r and s where r != s
+// i.e. join trees with roots r and s
+void DSets::join(int r, int s)
+{
+    assert(r != s);
+    assert(r >= 1 && r <= size);
+    assert(s >= 1 && s <= size);
+    assert(array[r] < 0);
+    assert(array[s] < 0);
+
+    // simple union
+    //array[r]  = s;
+
+    // *** TODO ***
+    // weighted union (by size)
+
+    //1. find the size of the tree -> the value of the root
+    int root1 = find(r);
+    int root2 = find(s);
+
+    //Root has the negative value of the number of nodes in the tree
+    //Compare the number of roots and append the tree with smaller size to the root of the other tree.
+    if(root1 < root2){
+        array[root1] += array[root2];
+        array[root2] = root1;
+    }
+    else{
+        array[root2] += array[root1];
+        array[root1] = root2;
+    }
+}
+
+// return name of current set for x
+// i.e. return root of tree for x
+int DSets::find(int x)
+{
+    assert(x >= 1 && x <= size);
+
+    // simple find
+    if (array[x] < 0)
+    {
+        return x;
+    }
+    else
+    {
+        return array[x] = find(array[x]);
+    }
+}
+
+// just in case ...
+void DSets::print() const
+{
+    cout << endl;
+    for (int i = 1; i <= size; i++)
+    {
+        cout << setw(4) << i;
+    }
+    cout << endl;
+    for (int i = 1; i <= size; i++)
+    {
+        cout << setw(4) << array[i];
+    }
+    cout << endl;
+}
